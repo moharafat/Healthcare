6f9be95ad158f2de70ecbd5cbe105ae73e388696
Mohamed Arafat <mohamedamr0001@gmail.com>
2024-07-06 14:50:47 +0300

    Amenities and states table 1 & removed 3 doctor spec

--- app.js
+++ app.js
@@ -6,35 +6,35 @@ menu.addEventListener('click',function() {
     menuLinks.classList.toggle('active');
 });
 
-// // location
+// location
 
-// document.getElementById('location-button').addEventListener('click', function() {
-//     if (navigator.geolocation) {
-//         navigator.geolocation.getCurrentPosition(showPosition, showError);
-//     } else {
-//         alert("Geolocation is not supported by this browser.");
-//     }
-// });
+document.getElementById('location-button').addEventListener('click', function() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
+});
 
-// function showPosition(position) {
-//     const lat = position.coords.latitude;
-//     const lon = position.coords.longitude;
-//     document.getElementById('location-input').value = `Latitude: ${lat}, Longitude: ${lon}`;
-// }
+function showPosition(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    document.getElementById('location-input').value = `Latitude: ${lat}, Longitude: ${lon}`;
+}
 
-// function showError(error) {
-//     switch(error.code) {
-//         case error.PERMISSION_DENIED:
-//             alert("User denied the request for Geolocation.");
-//             break;
-//         case error.POSITION_UNAVAILABLE:
-//             alert("Location information is unavailable.");
-//             break;
-//         case error.TIMEOUT:
-//             alert("The request to get user location timed out.");
-//             break;
-//         case error.UNKNOWN_ERROR:
-//             alert("An unknown error occurred.");
-//             break;
-//     }
-// }
+function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            alert("User denied the request for Geolocation.");
+            break;
+        case error.POSITION_UNAVAILABLE:
+            alert("Location information is unavailable.");
+            break;
+        case error.TIMEOUT:
+            alert("The request to get user location timed out.");
+            break;
+        case error.UNKNOWN_ERROR:
+            alert("An unknown error occurred.");
+            break;
+    }
+}
